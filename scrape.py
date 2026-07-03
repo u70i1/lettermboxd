021af03444a4a888f7928b21ca6c7123d1e8e627
u7o01 <1hasbi.ashshiddiq@gmail.com>
2026-07-03 08:31:15 +0700

    change entry item access method to .get() i think this method is safer since it returns None if the keys somehow missing

--- scrape.py
+++ scrape.py
@@ -1,7 +1,8 @@
 import feedparser
 import re
 
-def parse(username, page, amount_per_page):
+
+def parse(username: str, page: int, amount_per_page: int):
     start = (page - 1) * amount_per_page
     end = start + amount_per_page
 
@@ -11,17 +12,20 @@ def parse(username, page, amount_per_page):
 
     clean_entry = []
     for e in entry:
-        summary = re.sub(r"<.*?>", "", e.summary).strip() # type: ignore
-        clean_entry.append({
-            "link": e.link,
-            "watched_date": e.letterboxd_watcheddate,
-            "film_title": e.letterboxd_filmtitle,
-            "film_year": e.letterboxd_filmyear,
-            "member_rating": e.letterboxd_memberrating,
-            "member_like": True if e.letterboxd_watcheddate == "Yes" else False,
-            "member_rewatch": True if e.letterboxd_rewatch == "Yes" else False,
-            "tmbd_movieid": e.tmdb_movieid,
-            "review": summary
-        })
+        summary = re.sub(r"<.*?>", "", e.summary).strip()  # type: ignore
+        clean_entry.append(
+            {
+                "link": e.get("link"),
+                "watched_date": e.get("letterboxd_watcheddate"),
+                "film_title": e.get("letterboxd_filmtitle"),
+                "film_year": e.get("letterboxd_filmyear"),
+                "member_rating": e.get("letterboxd_memberrating"),
+                "member_like": True if e.get("letterboxd_watcheddate") == "Yes" else False,
+                "member_rewatch": True if e.get("letterboxd_rewatch") == "Yes" else False,
+                "tmbd_movieid": e.get("tmdb_movieid"),
+                "review": summary,
+            }
+        )
 
     return clean_entry
+
